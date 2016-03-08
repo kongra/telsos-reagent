a669334fb1d7c22ddd190159f8f7b740dee54256
Konrad Grzanek <kongra@gmail.com>
2016-03-08 20:05:40 +0100

    Added local Bootstrap/jQuery.

--- src/retemp/core.cljs
+++ src/retemp/core.cljs
@@ -1,5 +1,5 @@
 ;; Copyright (c) Konrad Grzanek. All rights reserved.
-;; Created 2015-11-12
+;; Created 2016-03-07
 
 (ns ^:figwheel-always retemp.core
     (:require [reagent.core :as r]))
@@ -8,19 +8,26 @@
 
 ;; VIEW
 
+(def counter (r/atom 0))
+
 (defn main-view
   []
-  [:h2.sample "Testowy napis"])
+  [:div.container
+   [:h2 (str "Kliknąłeś " @counter " razy.")]
+   [:button.btn.btn-success {:on-click #(swap!  counter inc) :type "button"} "Zwiększ"]
+   [:button.btn.btn-danger  {:on-click #(reset! counter 0  ) :type "button"} "Zeruj"  ]])
 
 
 ;; INSTRUMENTATION
 
-(defn on-js-reload []
+(defn on-js-reload
+  []
   (println "Reloaded...")
   (r/render-component [main-view] (. js/document (getElementById "app"))))
 
 
-(defn init []
+(defn init
+  []
   (on-js-reload))
 
 
