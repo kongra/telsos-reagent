2be1ccaf5b674517913ed554c532df3e99c11817
Konrad Grzanek <kongra@gmail.com>
2017-04-09 11:30:02 +0200

    -

--- src/retemp/core.cljs
+++ src/retemp/core.cljs
@@ -2,9 +2,80 @@
 ;; Created 2016-03-07
 
 (ns ^:figwheel-always retemp.core
-    (:require [reagent.core :as r]))
+  (:require [reagent.core    :as     r]
+            [cljs.core.async :refer [<! >! put! chan timeout]])
+
+  (:require-macros [cljs.core.async.macros :refer [go alt! go-loop]]))
 
 (enable-console-print!)
+
+;; (let [c (chan)]
+;;   (go
+;;     (.log js/console "--2-start")
+;;     (>! c 15)
+;;     (>! c 16)
+;;     (.log js/console "--2-end"))
+
+;;   (go
+;;     (.log js/console "--1-start")
+;;     (.log js/console (<! c))
+;;     (.log js/console (<! c))
+;;     (.log js/console "--1-end")))
+
+;; (go
+;;   (.log js/console "Hello")
+;;   (<! (timeout 2000))
+;;   (.log js/console "async")
+;;   (<! (timeout 2000))
+;;   (.log js/console "world!"))
+
+#_ (let [c (chan)
+      t (timeout 2000)]
+  (go
+    (alt!
+      c (.log js/console "Odebrałem wartość z kanału c")
+      t (.log js/console "Upłynął czas 2s")))
+
+  (go
+    (>! c 23)))
+
+#_ (let [c (chan)
+      t (timeout 2000)]
+  (go
+    (alt!
+      c ([v] (.log js/console (str "Odebrałem wartość " v " z kanału c")))
+      t ([v] (.log js/console (str "Upłynął czas 2s po których odebrałem " v)))))
+
+  ;; (go (<! (timeout 1000)) (>! c 23))
+  ;; (go (<! (timeout  900)) (>! c 24))
+  ;; (go (<! (timeout  200)) (>! c 25))
+  ;; (go (<! (timeout  356)) (>! c 26))
+
+  (go (<! (timeout 1000)) (>! c 23)
+      (<! (timeout  900)) (>! c 24)
+      (<! (timeout  200)) (>! c 25)
+      (<! (timeout  356)) (>! c 26)))
+
+#_ (let [c (chan)]
+  (go-loop []
+    (.log js/console (str "Odczytałem " (<! c)))
+    (recur))
+
+  (go (<! (timeout 1000)) (>! c 23)
+      (<! (timeout  900)) (>! c 24)
+      (<! (timeout  200)) (>! c 25)
+      (<! (timeout  356)) (>! c 26)))
+
+(defn events-chan
+  [element event-type]
+  (let [c (chan 100)]
+    (.addEventListener element event-type (fn [e] (put! c e)))
+    c))
+
+(let [mouse-c (events-chan js/window "mousemove")]
+  (go
+    (while true
+      (.log js/console (<! mouse-c)))))
 
 ;; VIEW
 
