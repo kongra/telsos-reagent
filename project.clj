ea58719e680f6ee15c4af366f13602589579623f
kongra <kongra@gmail.com>
2017-11-22 10:35:46 +0100

    Libraries update

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure       "1.8.0"  ]
-                 [org.clojure/clojurescript "1.9.908"]
-                 [org.clojure/core.async    "0.3.443"]
+                 [org.clojure/clojurescript "1.9.946"]
+                 [org.clojure/core.async    "0.3.465"]
                  [reagent                   "0.7.0"  ]]
 
   :plugins      [[lein-cljsbuild            "1.1.4"]
