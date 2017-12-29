99b9e89d587a010cc023eb0c4fd3c1b76cd3f31d
kongra <kongra@gmail.com>
2017-12-29 12:54:07 +0100

    Moved to clojure 1.9.0.

--- src/retemp/core.cljs
+++ src/retemp/core.cljs
@@ -8,109 +8,108 @@
 
 ;; VIEW
 
-;; (defn hidbutton
-;;   [label]
-;;   (let [isHidden (r/atom false)]
-;;     (fn []
-;;       (when-not @isHidden
-;;         [:button.btn.btn-warning
-;;          {:on-click #(reset! isHidden true) :type "button"} label]))))
+(defn hidbutton
+  [label]
+  (let [isHidden (r/atom false)]
+    (fn []
+      (when-not @isHidden
+        [:button.btn.btn-warning
+         {:on-click #(reset! isHidden true) :type "button"} label]))))
 
-;; (def ^:private counter (r/atom 0))
+(def ^:private counter (r/atom 0))
 
-;; (defn main-view []
-;;   [:div.container
-;;    [:h2 (str "You've clicked " @counter " times.")]
-;;    [:button.btn.btn-success {:on-click #(swap!  counter inc) :type "button"} "Add One"]
-;;    [:button.btn.btn-danger  {:on-click #(reset! counter   0) :type "button"} "Zero"]
-;;    [hidbutton "Hide me !!!"]
-;;    [hidbutton   "Noooo !!!"]])
+(defn main-view []
+  [:div.container
+   [:h2 (str "You've clicked " @counter " times.")]
+   [:button.btn.btn-success {:on-click #(swap!  counter inc) :type "button"} "Add One"]
+   [:button.btn.btn-danger  {:on-click #(reset! counter   0) :type "button"} "Zero"]
+   [hidbutton "Hide me !!!"]
+   [hidbutton "Hide me too !!!"]])
 
-;; ;; INSTRUMENTATION
+;; INSTRUMENTATION
 
-;; (defn on-js-reload
-;;   []
-;;   (println "Reloaded...")
-;;   (r/render-component [main-view] (. js/document (getElementById "app"))))
+(defn on-js-reload
+  []
+  (println "Reloaded...")
+  (r/render-component [main-view] (. js/document (getElementById "app"))))
 
-;; (defn init
-;;   []
-;;   (on-js-reload))
+(defn init
+  []
+  (on-js-reload))
 
-;; (defonce start
-;;   (init))
+(defonce start
+  (init))
 
-
-;; ASYNC
-;;
-(let [c (chan)]
-  (go
-    (.log js/console "--2-start")
-    (>! c 15)
-    (>! c 16)
-    (.log js/console "--2-end"))
-
-  (go
-    (.log js/console "--1-start")
-    (.log js/console (<! c))
-    (.log js/console (<! c))
-    (.log js/console "--1-end")))
-
-;; (go
-;;   (.log js/console "Hello")
-;;   (<! (timeout 2000))
-;;   (.log js/console "async")
-;;   (<! (timeout 2000))
-;;   (.log js/console "world!"))
-
-;; (let [c (chan)
-;;       t (timeout 2000)]
+;; ;; ASYNC
+;; ;;
+;; (let [c (chan)]
 ;;   (go
-;;     (alt!
-;;       c (.log js/console "Odebrałem wartość z kanału c")
-;;       t (.log js/console "Upłynął czas 2s")))
+;;     (.log js/console "--2-start")
+;;     (>! c 15)
+;;     (>! c 16)
+;;     (.log js/console "--2-end"))
 
 ;;   (go
-;;     (<! (timeout 3000))
-;;     (>! c 23)
-;;     ))
+;;     (.log js/console "--1-start")
+;;     (.log js/console (<! c))
+;;     (.log js/console (<! c))
+;;     (.log js/console "--1-end")))
 
-;; (let [c (chan)
-;;       t (timeout 2000)]
-;;   (go
-;;     (alt!
-;;       c ([v] (.log js/console (str "Odebrałem wartość " v " z kanału c")))
-;;       t ([v] (.log js/console (str "Upłynął czas 2s po których odebrałem " v)))))
+;; ;; (go
+;; ;;   (.log js/console "Hello")
+;; ;;   (<! (timeout 2000))
+;; ;;   (.log js/console "async")
+;; ;;   (<! (timeout 2000))
+;; ;;   (.log js/console "world!"))
 
-;;   ;; (go (<! (timeout 1000)) (>! c 23))
-;;   ;; (go (<! (timeout  900)) (>! c 24))
-;;   ;; (go (<! (timeout  200)) (>! c 25))
-;;   ;; (go (<! (timeout  356)) (>! c 26))
+;; ;; (let [c (chan)
+;; ;;       t (timeout 2000)]
+;; ;;   (go
+;; ;;     (alt!
+;; ;;       c (.log js/console "Odebrałem wartość z kanału c")
+;; ;;       t (.log js/console "Upłynął czas 2s")))
 
-;;   (go (<! (timeout 3000)) (>! c 23)
+;; ;;   (go
+;; ;;     (<! (timeout 3000))
+;; ;;     (>! c 23)
+;; ;;     ))
+
+;; ;; (let [c (chan)
+;; ;;       t (timeout 2000)]
+;; ;;   (go
+;; ;;     (alt!
+;; ;;       c ([v] (.log js/console (str "Odebrałem wartość " v " z kanału c")))
+;; ;;       t ([v] (.log js/console (str "Upłynął czas 2s po których odebrałem " v)))))
+
+;; ;;   ;; (go (<! (timeout 1000)) (>! c 23))
+;; ;;   ;; (go (<! (timeout  900)) (>! c 24))
+;; ;;   ;; (go (<! (timeout  200)) (>! c 25))
+;; ;;   ;; (go (<! (timeout  356)) (>! c 26))
+
+;; ;;   (go (<! (timeout 3000)) (>! c 23)
+;; ;;       (<! (timeout  900)) (>! c 24)
+;; ;;       (<! (timeout  200)) (>! c 25)
+;; ;;       (<! (timeout  356)) (>! c 26)))
+
+;; #_ (let [c (chan)]
+;;   (go-loop []
+;;     (.log js/console (str "Odczytałem " (<! c)))
+;;     (recur))
+
+;;   (go (<! (timeout 1000)) (>! c 23)
 ;;       (<! (timeout  900)) (>! c 24)
 ;;       (<! (timeout  200)) (>! c 25)
 ;;       (<! (timeout  356)) (>! c 26)))
 
-#_ (let [c (chan)]
-  (go-loop []
-    (.log js/console (str "Odczytałem " (<! c)))
-    (recur))
+;; #_ (defn events-chan
+;;   [element event-type]
+;;   (let [c (chan 100)]
+;;     (.addEventListener element event-type (fn [e] (put! c e)))
+;;     c))
 
-  (go (<! (timeout 1000)) (>! c 23)
-      (<! (timeout  900)) (>! c 24)
-      (<! (timeout  200)) (>! c 25)
-      (<! (timeout  356)) (>! c 26)))
+;; #_ (let [mouse-c (events-chan js/window "mousemove")]
+;;   (go
+;;     (while true
+;;       (.log js/console (<! mouse-c)))))
 
-#_ (defn events-chan
-  [element event-type]
-  (let [c (chan 100)]
-    (.addEventListener element event-type (fn [e] (put! c e)))
-    c))
-
-#_ (let [mouse-c (events-chan js/window "mousemove")]
-  (go
-    (while true
-      (.log js/console (<! mouse-c)))))
-
-;; VIEW
+;; ;; VIEW
