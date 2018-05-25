33405df09abeaf3ac0bf126c7416938888c1411b
kongra <kongra@gmail.com>
2018-05-25 15:10:59 +0200

    Libraries update.

--- project.clj
+++ project.clj
@@ -4,10 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure       "1.9.0"  ]
-                 [reagent                   "0.7.0"  ]
-                 [org.clojure/clojurescript "1.9.946"]
-                 [org.clojure/core.async    "0.4.474"]]
+  :dependencies [[org.clojure/clojure          "1.9.0"]
+                 [reagent                      "0.8.1"]
+                 [org.clojure/clojurescript "1.10.238"]
+                 [org.clojure/core.async     "0.4.474"]]
 
   :plugins      [[lein-cljsbuild            "1.1.7"]
                  [lein-figwheel             "0.5.14"]]
