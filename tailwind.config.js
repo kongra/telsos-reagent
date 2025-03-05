803f2d2863d24a53c981c3b2e31b16e87890d996
Konrad Grzanek <kongra@gmail.com>
2025-03-05 21:40:44 +0100

    Added JS parsing to tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  content: ["./src/**/*.cljs"],
+  content: ["./src/**/*.cljs", "./resources/public/assets/js/**/*.js"],
   theme: {
     extend: {},
   },
   corePlugins: {
-    preflight: true
-  }
-}
+    preflight: true,
+  },
+};
